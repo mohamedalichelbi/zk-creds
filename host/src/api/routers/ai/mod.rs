d8fd754dca08ef4ef1f23c0c4a02dc88869ad271
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2023-09-17 23:22:34 +0200

    provide context and example to gpt genscript

--- host/src/api/routers/ai/mod.rs
+++ host/src/api/routers/ai/mod.rs
@@ -36,36 +36,69 @@ pub struct GenScriptArgs {
 // generate a script with chatGPT
 pub async fn gen_script(Json(payload): Json<GenScriptArgs>) -> (StatusCode, Json<GenScriptResponse>) {
     let ai_client = Client::new();
+    
+    let mut msgs: Vec<ChatCompletionRequestMessage> = Vec::with_capacity(4);
+    // system message
+    msgs.push(ChatCompletionRequestMessage {
+        role: Role::System,
+        content: Option::Some(format!(
+            "You are a helpful code generation tool. Only reply with code in the {:?} language",
+            to_string(&payload.lang).unwrap(),
+        )),
+        name: Option::None,
+        function_call: Option::None
+    });
+    // example task
+    msgs.push(ChatCompletionRequestMessage {
+        role: Role::User,
+        content: Option::Some(format!(r#"
+            ```credentials = [{{"age": {{"type": "number"}}}}, {{"data": {{"type": "object", "properties": {{"country": {{"type": "string"}}}}}}}}]```.
+            Above are the descriptions of some objects in a variable of type Array called "credentials".
+            Assume the "credentials" variable is defined and constant. Assume the objects in it are JSON parsed.
+            Write a script in {:?} that only returns true if the user credentials satisfy the following requirements:
+            Age is at least 18, and country is Germany.
+            Write the shortest and most performant script. Only use variables if necessary, and give them single letter names.
+            "#,
+            to_string(&payload.lang).unwrap(),
+        )),
+        name: Option::None,
+        function_call: Option::None
+    });
+    // example expected response
+    msgs.push(ChatCompletionRequestMessage {
+        role: Role::Assistant,
+        content: Option::Some( match payload.lang {
+            ScriptLang::Rhai => r#"
+                (credentials[0]["age"] >= 18) && (credentials[1]["data"]["country"] == "Germany")
+                "#.to_string(),
+            ScriptLang::JavaScript => r#"
+                (credentials[0]["age"] >= 18) && (credentials[1]["data"]["country"] == "Germany")
+                "#.to_string(),
+        }),
+        name: Option::None,
+        function_call: Option::None
+    });
+    // actual task
+    msgs.push(ChatCompletionRequestMessage {
+        role: Role::User,
+        content: Option::Some(format!(r#"
+            Please do it again, but with different credentials and requirements:
+            ```credentials = [{:?}]```.
+            requirements:{}.
+            Assume the "credentials" variable is defined and constant. Assume the objects in it are JSON parsed.
+            "#,
+            payload.cred_schemes.join(","),
+            &payload.requirements,
+        )),
+        name: Option::None,
+        function_call: Option::None
+    });
+
+    println!("{:?}", &msgs);
+
     let request = CreateChatCompletionRequestArgs::default()
-        .model("gpt-3.5-turbo")
-        .messages(vec![
-            ChatCompletionRequestMessage {
-                role: Role::System,
-                content: Option::Some(format!(
-                    "You are a helpful code generation tool for the {:?} language",
-                    to_string(&payload.lang))
-                ),
-                name: Option::None,
-                function_call: Option::None
-            },
-            ChatCompletionRequestMessage {
-                role: Role::User,
-                content: Option::Some(format!(r#"
-                    ```credentials = [{:?}]```.
-                    Above are the descriptions of some objects in a variable of type Array called "credentials".
-                    Assume the "credentials" variable is defined.
-                    Write a script in {:?} that only returns true if the user credentials satisfy the following requirements:
-                    {}.
-                    Write the shortest and most performant script. Only use variables if necessary, and give them single letter names.
-                    "#,
-                    payload.cred_schemes.join(","),
-                    to_string(&payload.lang),
-                    &payload.requirements,
-                )),
-                name: Option::None,
-                function_call: Option::None
-            }
-        ])
+        .model("gpt-4")
+        .messages(msgs)
         .build()
         .unwrap();
     let response = ai_client.chat().create(request).await.unwrap();
